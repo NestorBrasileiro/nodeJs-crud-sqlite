82688b68391d42040b0367ca487989d67b6d83ac
NestorBrasileiro <nestorcruzbrasileiro@gmail.com>
2021-07-05 18:15:59 -0300

    Tratativa dos dados como válidos ou não e método de inserção de usuários no banco de dados

---
+++ src/services/CreateUserService.js
@@ -0,0 +1,40 @@
+
+const DataBase = require('../database/db');
+const User = require('../model/User');
+
+
+class CreateUserService{
+    async execute({nome, email, idade}){
+
+        
+        if(!nome || !email || !idade){
+            const err = new Error("All fields are mandatory");
+            err.status = 400;
+            err.expose = true;
+            throw err;
+        }
+        if(idade < 18){
+            const err = new Error("Can't save users under 18 years old");
+            err.status = 400;
+            err.expose = true;
+            throw err;
+        }
+
+        try{
+            await DataBase.sync();
+        }
+        catch(err){
+            console.log(err);
+        }
+        const createNewUser = await User.create({
+            nome: nome,
+            email: email,
+            idade: idade
+        })
+        return createNewUser;
+        
+    }
+    
+
+}
+module.exports = CreateUserService;
