b18242022449efcf686307b0171b603d332a3801
NestorBrasileiro <nestorcruzbrasileiro@gmail.com>
2021-07-05 17:56:19 -0300

    adicionando middleware de inserção de Usuário

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,15 @@
+require('dotenv').config();
 const Router = require('koa-router');
-
 const router = new Router();
+const CreateUserController = require('./controller/CreateUserController')
 
-//rota simples pra testar se o servidor está online
+const createUserController = new CreateUserController();
+
+
 router.get('/', async (ctx) => {
-  ctx.body = `Seu servidor esta rodando em http://localhost:3000`; //http://localhost:3000/
+  ctx.response = `Seu servidor esta rodando em http://localhost:${process.env.PORT}`; //http://localhost:3000/
 });
 
-//Uma rota de exemplo simples aqui.
-//As rotas devem ficar em arquivos separados, /src/controllers/userController.js por exemplo
-router.get('/users', async (ctx) => {
-    ctx.status = 200;
-    ctx.body = {total:0, count: 0, rows:[]}
-});
+router.post('/users', createUserController.handle);
 
 module.exports = router;
