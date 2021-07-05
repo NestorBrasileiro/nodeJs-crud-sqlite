7cfc7e7ba60836e379630eac710d3870c42b798e
NestorBrasileiro <nestorcruzbrasileiro@gmail.com>
2021-07-05 17:54:48 -0300

    adicionando processamento de erros

--- src/index.js
+++ src/index.js
@@ -5,15 +5,40 @@
 //https://github.com/ZijianHe/koa-router
 
 // todas as configuraçoes devem ser passadas via environment variables
+require('dotenv').config();
 
 const Koa = require('koa');
-const router = require('./routes')
+const router = require('./routes');
+const json = require('koa-json');
+const bodyParser = require('koa-bodyparser');
+const error = require('koa-json-error');
+
 
 const koa = new Koa();
 const PORT = process.env.PORT || 3000;
 
 
-koa.use(router.routes());
+function formatError(err){
+  if(err instanceof Error){
+    return {
+      erro: err.message
+    }
+
+  }
+  else{
+    return {
+      message: 'internal server error'
+    }
+  }
+  
+}
+
+
+
+koa.use(json());
+koa.use(bodyParser());
+koa.use(error(formatError))
+koa.use(router.routes()).use(router.allowedMethods());
 
 const server = koa.listen(PORT,() => {
   console.log("Server is Running");
