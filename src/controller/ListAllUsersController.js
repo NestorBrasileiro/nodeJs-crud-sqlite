29aa9c6b00c255da24d5167e9c5749fb6be783b1
NestorBrasileiro <nestorcruzbrasileiro@gmail.com>
2021-07-06 18:17:40 -0300

    removendo as informações não usadas por esta rota

--- src/controller/ListAllUsersController.js
+++ src/controller/ListAllUsersController.js
@@ -3,16 +3,9 @@ const ListAllUsersService = require('../services/ListAllUsersService')
 class ListAllUsersController{
     async handle(ctx){
         
-        const listAllUserService = new ListAllUserService();
-        
-        
-        const nome = ctx.request.params.nome;
-        console.log(nome);
-
+        const listAllUserService = new ListAllUsersService();
         const user = await listAllUserService.execute();
-
         return ctx.response.body = {user}; 
-
     }
 }
 module.exports = ListAllUsersController;
