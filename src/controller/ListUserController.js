32c63cb59f79386edbcd29d125d62110e0c6a5a7
NestorBrasileiro <nestorcruzbrasileiro@gmail.com>
2021-07-06 15:21:35 -0300

    Removendo parametros de busca, deixando apenas o nome

--- src/controller/ListUserController.js
+++ src/controller/ListUserController.js
@@ -2,10 +2,19 @@ const ListUserService = require('../services/ListUserService')
 
 class ListUserController{
     async handle(ctx){
-
-        const listUserService = new ListUserService();
         
-        const user = await listUserService.execute()
+        const listUserService = new ListUserService();
+
+        if(!ctx.request.body){
+            const allUsers = await listUserService.execute();
+            return ctx.response.body = {allUsers};
+        }
+
+        //OBS: Também da pra buscar pela query do navegador, se for usar o ctx.request.body
+
+        const {nome,} = ctx.request.body;
+        
+        const user = await listUserService.execute({nome});
 
         return ctx.response.body = {user}; 
 
