87b61b63258e250e7a0153d9f4c279241394ea12
NestorBrasileiro <nestorcruzbrasileiro@gmail.com>
2021-07-06 17:31:40 -0300

    Adicionando a rota get users que listará todos os usuários

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,12 @@ const router = new Router();
 const CreateUserController = require('./controller/CreateUserController');
 const ListUserController = require('./controller/ListUserController');
 const UpdateUserController = require('./controller/UpdateUserController');
+const ListAllUsersController = require('./controller/ListAllUsersController');
 
 const createUserController = new CreateUserController();
 const listUserController = new ListUserController();
 const updateUserController = new UpdateUserController();
+const listAllUsersController = new ListAllUsersController();
 
 
 
@@ -18,6 +20,7 @@ router.get('/', async (ctx) => {
 
 router.post('/users', createUserController.handle);
 router.get('/user/:nome', listUserController.handle);
+router.get('/users', listAllUsersController.handle);
 router.patch('/users', updateUserController.handle);
 
 module.exports = router;
