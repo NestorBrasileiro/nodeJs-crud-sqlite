be63543f386351c80806fef53ebe0c2437a3d3c2
NestorBrasileiro <nestorcruzbrasileiro@gmail.com>
2021-07-06 18:12:45 -0300

    Adicionando route delete, e ajuste na rota post "user", estava "users"

--- src/routes.js
+++ src/routes.js
@@ -6,21 +6,26 @@ const CreateUserController = require('./controller/CreateUserController');
 const ListUserController = require('./controller/ListUserController');
 const UpdateUserController = require('./controller/UpdateUserController');
 const ListAllUsersController = require('./controller/ListAllUsersController');
+const DeleteUserController = require('./controller/DeleteUserController');
 
 const createUserController = new CreateUserController();
 const listUserController = new ListUserController();
 const updateUserController = new UpdateUserController();
 const listAllUsersController = new ListAllUsersController();
+const deleteUserController = new DeleteUserController();
 
 
 
 router.get('/', async (ctx) => {
-  ctx.response = `Seu servidor esta rodando em http://localhost:${process.env.PORT}`; //http://localhost:3000/
+  ctx.response.body = {
+    message: `Seu servidor esta rodando em http://localhost:${process.env.PORT}`
+  }
 });
 
-router.post('/users', createUserController.handle);
+router.post('/user', createUserController.handle);
 router.get('/user/:nome', listUserController.handle);
 router.get('/users', listAllUsersController.handle);
 router.patch('/users', updateUserController.handle);
+router.delete("/user/:name", deleteUserController.handle);
 
 module.exports = router;
