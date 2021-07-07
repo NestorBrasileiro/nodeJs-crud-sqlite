b90d1c1f23db2d3dc9fe11d0809cc80565d0a43c
NestorBrasileiro <nestorcruzbrasileiro@gmail.com>
2021-07-06 22:21:05 -0300

    refatorando

--- src/database/db.js
+++ src/database/db.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const DB_FILEPATH = process.env.DB_FILEPATH || ':memory:'
+const DB_FILEPATH = process.env.DB_FILEPATH
 const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
