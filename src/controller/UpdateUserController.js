b2a80c0411ccf2a54ed0d70fdf0124b6993f7647
NestorBrasileiro <nestorcruzbrasileiro@gmail.com>
2021-07-06 22:19:04 -0300

    Refatoramento

--- src/controller/UpdateUserController.js
+++ src/controller/UpdateUserController.js
@@ -4,11 +4,8 @@ class UpdateUserController{
     async handle(ctx){
         
         const updateUserService = new UpdateUserService();
-
         const {nome, email, idade} = ctx.request.body;
-        
         const user = await updateUserService.execute({nome, email, idade});
-
         return ctx.response.body = {user};
     }
 }
