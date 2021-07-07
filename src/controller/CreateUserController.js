df447d132cc603657aa98cecfeaf40f23dd56082
NestorBrasileiro <nestorcruzbrasileiro@gmail.com>
2021-07-06 22:14:11 -0300

    adicionando o retorno do status para 201

--- src/controller/CreateUserController.js
+++ src/controller/CreateUserController.js
@@ -3,19 +3,15 @@ const CreateUserService = require('../services/CreateUserService')
 class CreateUserController{
     async handle(ctx){
 
-
         const {nome,email, idade} = ctx.request.body
-        
         const createUserService = new CreateUserService();
-        
         const user = await createUserService.execute({
             nome,
             email,
             idade
         })
-
+            ctx.response.status = 201;
         return ctx.response.body = {user};
-
     }
 }
 module.exports = CreateUserController;
