f195c1aa8f22348347c5ef3688705c2c2a384b1a
NestorBrasileiro <nestorcruzbrasileiro@gmail.com>
2021-07-06 22:23:38 -0300

    Removendo o ID, e adicionando as propriedades unique e required

--- src/model/User.js
+++ src/model/User.js
@@ -2,23 +2,22 @@ const database = require('../database/db');
 const Sequelize = require('sequelize')
 
 const User = database.define('User', {
-    id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true
-    },
     nome: {
         type: Sequelize.STRING(100),
-        allowNull: false
+        allowNull: false,
+        required: true
     },
     email: {
         type: Sequelize.STRING(70),
-        allowNull: false
+        allowNull: false,
+        required:true,
+        unique: true
     },
     idade: {
-        type: Sequelize.INTEGER,
-        allowNull: false
+        type: Sequelize.NUMBER,
+        allowNull: false,
+        minimum: 18,
+        required: true
     }    
 });
 
