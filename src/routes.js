22ab6958cb2709ea7e0abb44942a6d3f5d45ab00
NestorBrasileiro <nestorcruzbrasileiro@gmail.com>
2021-07-06 22:24:45 -0300

    Correção de erro na rota delete, e refatormção

--- src/routes.js
+++ src/routes.js
@@ -14,18 +14,15 @@ const updateUserController = new UpdateUserController();
 const listAllUsersController = new ListAllUsersController();
 const deleteUserController = new DeleteUserController();
 
-
-
 router.get('/', async (ctx) => {
   ctx.response.body = {
     message: `Seu servidor esta rodando em http://localhost:${process.env.PORT}`
   }
 });
-
 router.post('/user', createUserController.handle);
 router.get('/user/:nome', listUserController.handle);
 router.get('/users', listAllUsersController.handle);
 router.patch('/users', updateUserController.handle);
-router.delete("/user/:name", deleteUserController.handle);
+router.delete("/user/:nome", deleteUserController.handle);
 
 module.exports = router;
