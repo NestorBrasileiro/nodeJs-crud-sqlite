0ab0cb3ecec9d8ed6e4c86b59b756acd82746582
NestorBrasileiro <nestorcruzbrasileiro@gmail.com>
2021-07-06 22:17:39 -0300

    Refatoramento

--- src/controller/ListUserController.js
+++ src/controller/ListUserController.js
@@ -4,15 +4,9 @@ class ListUserController{
     async handle(ctx){
         
         const listUserService = new ListUserService();
-        
-        
         const nome = ctx.request.params.nome;
-        console.log(nome);
-
         const user = await listUserService.execute(nome);
-
         return ctx.response.body = {user}; 
-
     }
 }
 module.exports = ListUserController;
